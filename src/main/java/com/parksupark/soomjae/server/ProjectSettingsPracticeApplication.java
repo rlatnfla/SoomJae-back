619b2f6b05fec00737bfebd592a7a62e302b2f15
rlatnfla <rlatnfla0724@gmail.com>
2025-04-01 15:01:24 +0900

    [feat] 프로젝트 초기화

---
+++ src/main/java/com/parksupark/soomjae/server/ProjectSettingsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.parksupark.soomjae.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSettingsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSettingsPracticeApplication.class, args);
+    }
+
+}
